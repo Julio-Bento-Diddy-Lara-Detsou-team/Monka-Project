6974f4f46a1449e51fd63b459e03869d187ad4db
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-09 11:59:50 +0200

    [DB]Add-forgotten-file-schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_092041) do
+ActiveRecord::Schema.define(version: 2020_06_09_094603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_06_09_092041) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.integer "zip_code"
+    t.string "country", default: "FRANCE"
+    t.integer "phone_number"
+    t.integer "company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
