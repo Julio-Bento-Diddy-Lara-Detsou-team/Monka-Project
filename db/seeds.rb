277ded0eb9a5d53e846b3e8083e78449be7cb4f3
Bento <oobento01oo@gmail.com>
2020-06-09 15:49:09 +0200

    [DB] Edit seed for customers table

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,18 +27,18 @@ end
 
 # Populate customers
   nb_customers.times do |i|
-    Customers.create!(
+    Customer.create!(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         company_name: Faker::Company.name,
+        company_id: "36252187900034",
         address: Faker::Address.street_address,
         zip_code: Faker::Address.zip_code,
         country: "FRANCE",
-        phone_number: "0664765890",
+        phone_number: "0612131415",
         email: Faker::Name.first_name + "@yopmail.com",
-
-
+        is_professional: true
     )
 
-    puts "#{i+1} client(s) generated"
+    puts "#{i+1} customer(s) generated"
   end
