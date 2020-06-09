59aca21edfa38e794f6bc6bbb4b30e7fbf7129c0
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-09 11:58:25 +0200

    [DB]Add-elements-to-users

---
+++ db/migrate/20200609094603_add_elements_to_users.rb
@@ -0,0 +1,11 @@
+class AddElementsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :zip_code, :integer
+    add_column :users, :country, :string, default: "FRANCE"
+    add_column :users, :phone_number, :integer
+    add_column :users, :company_id, :integer
+  end
+end
