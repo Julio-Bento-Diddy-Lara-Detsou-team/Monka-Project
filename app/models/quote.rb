0cc2a21b88db42b5f85b57880e9c6f274b746095
Bento <oobento01oo@gmail.com>
2020-06-09 17:34:22 +0200

    [DB] Add validations to table Quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,16 @@
-class Quote < ApplicationRecord
+class Quote < ApplicationRecord  # Validations
+  validates :version_number,
+            presence: true
+
+  validates :amount,
+            presence: true
+
+  # validates :discount
+
+  # validates :sent
+
+  # validates :sending_counter
+
+  # validates :is_invoice
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 # Edit the variables below to control the number of items to generate in each table of the database
 nb_users = 1
-nb_customers =1
+nb_customers = 1
+nb_quotes = 1
 
 # Reset tables in the database
 User.destroy_all
 Customer.destroy_all
+Quote.destroy_all
 
 users = []
+customers = []
 
 # Populate users
 nb_users.times do |i|
@@ -29,7 +32,7 @@ nb_users.times do |i|
 
   # Populate customers
   nb_customers.times do |i|
-    Customer.create!(
+    customers << Customer.create!(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         company_name: Faker::Company.name,
@@ -45,4 +48,22 @@ nb_users.times do |i|
 
     puts "#{i+1} customer(s) generated"
   end
+end
+
+# Populate quotes
+nb_quotes.times do |i|
+
+  quoted_customer = customers.sample
+
+  Quote.create!(
+      version_number: "#{quoted_customer.first_name[0]}#{quoted_customer.last_name[0]}#{DateTime.now.strftime("%d%m%Y%H%M%S")}#{i}",
+      amount: rand(250..5000),
+      discount: 0,
+      sent: DateTime.now,
+      sending_counter: 0,
+      is_invoice: false
+      # user: users.sample
+  )
+
+  puts "#{i+1} quote(s) generated"
 end
