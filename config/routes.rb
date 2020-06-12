5c4ecbe43dd42ba7bc7a3e8091fb932dc68d574c
LaraSch <62933751+Laramarey@users.noreply.github.com>
2020-06-12 21:38:41 +0200

    [Front] Delete examples views related to Flowdash theme
    * [Front]cleaned views
    
    * [Front]errased uneffective pagination + errased routes and views from theme

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  get 'flowdash/:page' => 'flowdash#show', as: 'flowdash'
-
   resources 'home', only: [:index]
   resources 'quotes'
   resources 'users', only: [:show, :edit, :update]
