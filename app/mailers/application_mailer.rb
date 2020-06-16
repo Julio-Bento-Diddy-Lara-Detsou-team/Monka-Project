3c472db31d742ad9c458abe79badb762fc09c4ec
Laramarey <l.schutz@tbs-education.org>
2020-06-16 12:10:23 +0200

    [Back]Active mailer parametered with sendrig and api key

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,4 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'moufaca755@gmail.com'
   layout 'mailer'
 
-  # def cart_price(order)
-  #   @cart = order.user.cart
-  #   @cart_price = 0
-  #   @cart.items.each do |item|
-  #     @cart_price += item.price
-  #   end
-  #   @cart_price
-  # end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'https://monka-project.herokuapp.com//'
+    @url = 'https://monka-project.herokuapp.com'
     mail(to: @user.email, subject: 'Bienvenue chez Monka !')
   end
 
