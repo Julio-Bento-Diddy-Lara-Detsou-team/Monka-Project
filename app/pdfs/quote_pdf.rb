1c2dc462e8af9a1c80582a891f536862ed355035
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-16 14:13:18 +0200

    [Fullstack] Add destination and sender address in pdf

--- app/pdfs/quote_pdf.rb
+++ app/pdfs/quote_pdf.rb
@@ -2,7 +2,27 @@ class QuotePdf < Prawn::Document
   def initialize(quote)
     super(top_margin: 70)
     @quote = quote
-    text "Devis #{@quote.quote_number} "
+    quote_number
+    destination_address
+    sender_address
   end
+
+  def quote_number
+    text "Devis #{@quote.quote_number}"
+  end
+
+  def destination_address
+    text "#{@quote.customer.company_name}"
+    text "#{@quote.customer.address}"
+    text "#{@quote.customer.zip_code} #{@quote.customer.country}"
+  end
+
+  def sender_address
+
+  end
+
+
+
+
 
 end
