0b203235488056570a43ed628a37bbb3bf41bc53
Laramarey <l.schutz@tbs-education.org>
2020-06-16 10:07:40 +0200

    [Back]Active-mailer installed and filled application_mailer.rb and user_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'moufaca755@gmail.com'
   layout 'mailer'
+
+  # def cart_price(order)
+  #   @cart = order.user.cart
+  #   @cart_price = 0
+  #   @cart.items.each do |item|
+  #     @cart_price += item.price
+  #   end
+  #   @cart_price
+  # end
 end
