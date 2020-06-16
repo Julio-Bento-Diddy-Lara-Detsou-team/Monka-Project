f75329e146b699018f50dfe518f7c7aa27951ac0
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-16 10:52:51 +0200

    [back] Download pdf when add .pdf to quotes/id

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,6 +12,14 @@ class QuotesController < ApplicationController
 
   def show
     @goods = @quote.goods
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = Prawn::Document.new
+        pdf.text "Hello World"
+        send_data pdf.render
+      end
+    end
   end
 
   def new
