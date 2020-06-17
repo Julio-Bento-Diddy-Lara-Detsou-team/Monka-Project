fe4d5201b0d4426d48e228790d3a8ed5adf4fddb
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-17 13:56:17 +0200

    [Back] Add User and quotes to Admin Dashboard

---
+++ app/admin/goods.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Good do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :description, :quantity, :price, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :description, :quantity, :price, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/users.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :company_name, :address, :zip_code, :country, :phone_number, :company_id, :is_admin
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :company_name, :address, :zip_code, :country, :phone_number, :company_id, :is_admin]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
