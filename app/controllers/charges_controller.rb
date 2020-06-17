5ca309516d0b8ec64cc8ae23167b7cd09e044e05
Bento <oobento01oo@gmail.com>
2020-06-17 15:35:48 +0200

    [Back] Fix indentation in ChargesController

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,4 @@
 class ChargesController < ApplicationController
-
   def new
     @quote = Quote.find(params[:quote_id])
   end
@@ -10,22 +9,22 @@ class ChargesController < ApplicationController
     # Amount in cents
     @amount = @quote.amount.to_i * 100
 
-    customer = Stripe::Customer.create({
-                                           email: params[:stripeEmail],
-                                           source: params[:stripeToken],
-                                       })
+    customer = Stripe::Customer.create(
+        [
+            email: params[:stripeEmail],
+            source: params[:stripeToken]
+        ])
 
-    charge = Stripe::Charge.create({
-                                       customer: customer.id,
-                                       amount: @amount,
-                                       description: 'Rails Stripe customer',
-                                       currency: 'EUR',
-                                   })
+    charge = Stripe::Charge.create(
+        [
+            customer: customer.id,
+            amount: @amount,
+            description: 'Rails Stripe customer',
+            currency: 'EUR'
+        ])
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_quote_charge_path
   end
-
-
 end
