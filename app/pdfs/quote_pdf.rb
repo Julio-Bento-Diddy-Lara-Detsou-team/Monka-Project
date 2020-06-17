87078b0d1e56aadf80a6abcb6ce0f6edf074f9e4
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-17 11:28:38 +0200

    [Back] Cleaning code comments

--- app/pdfs/quote_pdf.rb
+++ app/pdfs/quote_pdf.rb
@@ -14,61 +14,44 @@ class QuotePdf < Prawn::Document
 
   def quote_number
     text_box "Devis #{@quote.quote_number}",
-    :at => [0, 725],
-        :height => 100,
-        :width => 150
+             :at => [0, 650],
+             :height => 100,
+             :width => 150
   end
 
   def destination_address
     text_box"#{@quote.customer.company_name} #{@quote.customer.address} #{@quote.customer.zip_code} #{@quote.customer.country}",
-            :at => [0, 700],
+            :at => [0, 600],
             :height => 100,
             :width => 150
   end
 
   def sender_address
     text_box"#{@user.company_name} #{@user.address} #{@user.zip_code} #{@user.country}",
-    :at => [400, 700],
+    :at => [400, 600],
         :height => 100,
         :width => 150
   end
 
   def show_goods
-    move_down 100
+    move_down 200
     table show_goods_rows do
       row(0).font_style = :bold
     end
-    # total_price = 0
-    # @quote.goods.each do |good|
-    #
-    #   data = [[" #{good.title}", "#{good.description}", "#{good.quantity}", " #{good.price} €" ]]
-    #   table(data, :column_widths => {1 => 240}, :position => :center)
-    #
-    #   data = [["Titre", "Description", "Quantité", "Prix"],
-    #           ["#{good.title}", "#{good.description}", "#{good.quantity}", " #{good.price} €" ]
-    #   ]
-    #   table(data, :column_widths => {1 => 240}, :position => :center)
-    #
-    #
-    #   # text "#{good.title}"
-    #   # text "#{good.description}"
-    #   # text "#{good.quantity}"
-    #   # text "#{good.price} €"
-    #   total_price = total_price + good.quantity * good.price
   end
 
-    # text "Prix Total #{total_price} €"
-
   def show_goods_rows
+
     data = [["Produit/Service", "Description", "Quantité", "prix"]]
     @quote.goods.each do |good|
+      total_price = 0
       data << [good.title, good.description, good.quantity, price(good.price)]
     end
     data
   end
 
   def terms_and_conditions
-    move_down 50
+    move_down 200
     text "Termes et conditions"
     text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse facilisis feugiat erat, sed porta lacus pellentesque mattis. Suspendisse id vulputate mi. Nunc posuere varius sapien et commodo. Praesent pharetra nibh nulla, vitae cursus tellus semper ut. Sed nisl diam, ultrices id maximus in, mollis eu sapien."
   end
