dbe67ff7447d92037f8a6cbd412447813a1d2ff7
didy400 <didy.and@gmail.com>
2020-06-19 11:38:46 +0200

    [Fullstack] Set url for forgotten password + pimp edit password view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.default_url_options = { :host => 'monka-project-staging.herokuapp.com' }
 end
