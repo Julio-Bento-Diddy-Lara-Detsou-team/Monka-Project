3d72d25da905024badcbe5226584da51c52789af
Bento <oobento01oo@gmail.com>
2020-10-08 16:05:56 +0200

    style: improve general style
    Improve buttons on CRUD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @invoices = Quote.all
-
     if current_user
+      @date = Time.now.strftime("%B %Y")
+      @invoices = Quote.all
+
       @paid_invoices = current_user.quotes.where(is_paid: true)
       @unpaid_invoices = current_user.quotes.where(is_paid: false, is_invoice: true)
+      @month_invoices = current_user.quotes.where(is_paid: true).where("quote_sending_date >= Now() OR invoice_sending_date >= Now()")
     end
   end
 
